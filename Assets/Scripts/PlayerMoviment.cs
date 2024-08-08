7c37a97ee1f76e22083ea3eefd86dff72dd73fa7
zazzymann <90633631+zazzymann@users.noreply.github.com>
2024-08-08 11:42:57 -0300

    Menu e Mapa

---
+++ Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    public float moveH;
+    public int velocidade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float moveH;
+        float velocidade = 5;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
